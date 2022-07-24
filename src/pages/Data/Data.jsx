44663c6f25564bb0af4460c50b8d283dfd696425
berserkersword <lamjarred063@gmail.com>
2022-07-24 08:33:57 -0400

    main

--- src/pages/Data/Data.jsx
+++ src/pages/Data/Data.jsx
@@ -11,7 +11,7 @@ const Data = ({ querry }) => {
                 {querry.results ?
                     querry.results.map(item => {
                         return (
-                            <a href={item.link} key={item.link} className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            <a href={item.link} key={item.link} className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 overflow-hidden">
                                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.title}</h5>
                                 <p className="font-normal text-gray-700 dark:text-gray-400">{item.description}</p>
                             </a>
